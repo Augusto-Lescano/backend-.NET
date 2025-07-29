6f9880b09fe50680909469f768772645b03e027a
Gian Marco <104176205+GianMarcoMercanzini@users.noreply.github.com>
2025-07-29 17:42:16 -0300

    crud Usuario (#4)

---
+++ proyTorneos/Domain.Services/UsuarioService.cs
@@ -0,0 +1,75 @@
+﻿using Domain.Model;
+using Data;
+
+namespace Domain.Services
+{
+    public class UsuarioService
+    {
+        public void Add(Usuario usuario)
+        {
+            usuario.SetId(GetNextId());
+
+            UsuarioInMemory.Usuarios.Add(usuario);
+        }
+
+        public bool Delete(int id)
+        {
+            Usuario? usuarioToDelete = UsuarioInMemory.Usuarios.Find(x => x.Id == id);
+
+            if (usuarioToDelete != null)
+            {
+                UsuarioInMemory.Usuarios.Remove(usuarioToDelete);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Usuario Get(int id)
+        {
+            return UsuarioInMemory.Usuarios.Find(x => x.Id == id);
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            return UsuarioInMemory.Usuarios.ToList();
+        }
+
+        public bool Update(Usuario usuario)
+        {
+            Usuario? usuarioToUpdate = UsuarioInMemory.Usuarios.Find(x => x.Id == usuario.Id);
+
+            if (usuarioToUpdate != null)
+            {
+                usuarioToUpdate.SetNombre(usuario.Nombre);
+                usuarioToUpdate.SetApellido(usuario.Apellido);
+                usuarioToUpdate.SetEmail(usuario.Email);
+                usuarioToUpdate.SetPais(usuario.Pais);
+                usuarioToUpdate.SetGamerTag(usuario.GamerTag);
+                usuarioToUpdate.SetRol(usuario.Rol);
+
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        private static int GetNextId()
+        {
+            int nextId;
+
+            if (UsuarioInMemory.Usuarios.Count > 0)
+            {
+                nextId = UsuarioInMemory.Usuarios.Max(x => x.Id) + 1;
+            }
+            else
+            {
+                nextId = 1;
+            }
+
+            return nextId;
+        }
+    }
+}
--- proyTorneos/WebAPI/Program.cs
+++ proyTorneos/WebAPI/Program.cs
@@ -1,6 +1,158 @@
+using Microsoft.AspNetCore.OpenApi;
+using System.ComponentModel.DataAnnotations;
+using Domain.Model;
+using Domain.Services;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpLogging(o => { });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //Falta configurar de manera correcta        
+    app.UseHttpLogging();
+}
+app.UseHttpsRedirection();
+
+app.MapGet("/usuarios/{id}", (int id) =>
+{
+    UsuarioService usuarioService = new UsuarioService(); 
+
+    Usuario usuario = usuarioService.Get(id);
+
+    if (usuario == null)
+    {
+        return Results.NotFound();
+    }
+        
+    var dto = new DTOs.Usuario
+    {
+        Id = usuario.Id,
+        Nombre = usuario.Nombre,
+        Apellido = usuario.Apellido,
+        Email = usuario.Email,
+        Pais = usuario.Pais,
+        GamerTag = usuario.GamerTag,
+        Rol = usuario.Rol
+    };
+
+    return Results.Ok(dto);
+})
+.WithName("GetUsuario")
+.Produces<DTOs.Usuario>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.WithOpenApi();
+
+app.MapGet("/Usuarios", () =>
+{
+    UsuarioService usuarioService1 = new UsuarioService();
+
+    var usuarios = usuarioService1.GetAll();
+
+    var dtos = usuarios.Select(usuario => new DTOs.Usuario
+    {
+        Id = usuario.Id,
+        Nombre = usuario.Nombre,
+        Apellido = usuario.Apellido,
+        Email = usuario.Email,
+        Pais = usuario.Pais,
+        GamerTag = usuario.GamerTag,
+        Rol = usuario.Rol
+    }).ToList();
+
+    return Results.Ok(dtos);
+})
+.WithName("GetAllUsuarios")
+.Produces<List<DTOs.Usuario>>(StatusCodes.Status200OK)
+.WithOpenApi();
+
+app.MapPost("/usuarios", (DTOs.Usuario dto) =>
+{
+    try
+    {
+        UsuarioService alumnoService = new UsuarioService();
+
+        Usuario usuario = new Usuario(dto.Id, dto.Nombre, dto.Apellido, dto.Email, dto.Pais, dto.GamerTag, dto.Rol);
+
+        alumnoService.Add(usuario);
+
+        var dtoResultado = new DTOs.Usuario
+        {
+            Id = usuario.Id,
+            Nombre = usuario.Nombre,
+            Apellido = usuario.Apellido,
+            Email = usuario.Email,
+            Pais = usuario.Pais,
+            GamerTag = usuario.GamerTag,
+            Rol = usuario.Rol
+        };
+
+        return Results.Created($"/usuarios/{dtoResultado.Id}", dtoResultado);
+    }
+    catch (ArgumentException ex)
+    {
+        return Results.BadRequest(new { error = ex.Message });
+    }
+})
+.WithName("AddUsuario")
+.Produces<DTOs.Usuario>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+app.MapPut("/usuarios/{id}", (int id, DTOs.Usuario dto) =>
+{
+    try
+    {
+        UsuarioService usuarioService = new UsuarioService();
+
+        Usuario usuario = new Usuario(id, dto.Nombre, dto.Apellido, dto.Email, dto.Pais, dto.GamerTag, dto.Rol);
+
+        var found = usuarioService.Update(usuario);
+
+        if (!found)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.NoContent();
+    }
+    catch (ArgumentException ex)
+    {
+        return Results.BadRequest(new { error = ex.Message });
+    }
+})
+.WithName("UpdateUsuario")
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+app.MapDelete("/usuario/{id}", (int id) =>
+{
+    UsuarioService usuarioService = new UsuarioService();
+
+    var deleted = usuarioService.Delete(id);
+
+    if (!deleted)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.NoContent();
+
+})
+.WithName("DeleteUsuario")
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status404NotFound)
+.WithOpenApi();
 
 app.Run();
