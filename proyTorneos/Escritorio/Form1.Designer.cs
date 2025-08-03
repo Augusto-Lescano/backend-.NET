a4b6f149ad44f2435599c080555d52b3b54376fb
Gian Marco <104176205+GianMarcoMercanzini@users.noreply.github.com>
2025-08-03 01:58:34 -0300

    formUsuario y menuPrincipal

--- proyTorneos/Escritorio/Form1.Designer.cs
+++ proyTorneos/Escritorio/Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panelMenu = new Panel();
+            btnTorneos = new Button();
+            btnUsuarios = new Button();
+            panelLogo = new Panel();
+            label1 = new Label();
+            panelTitleBar = new Panel();
+            btnMinimize = new Button();
+            btnMaximize = new Button();
+            btnClose = new Button();
+            btnCloseChildForm = new Button();
+            lblTitle = new Label();
+            panelDesktopPane = new Panel();
+            panelMenu.SuspendLayout();
+            panelLogo.SuspendLayout();
+            panelTitleBar.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(btnTorneos);
+            panelMenu.Controls.Add(btnUsuarios);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(207, 542);
+            panelMenu.TabIndex = 0;
+            // 
+            // btnTorneos
+            // 
+            btnTorneos.Dock = DockStyle.Top;
+            btnTorneos.FlatAppearance.BorderSize = 0;
+            btnTorneos.FlatStyle = FlatStyle.Flat;
+            btnTorneos.Font = new Font("Segoe UI", 10F);
+            btnTorneos.ForeColor = Color.Gainsboro;
+            btnTorneos.Image = (Image)resources.GetObject("btnTorneos.Image");
+            btnTorneos.ImageAlign = ContentAlignment.MiddleLeft;
+            btnTorneos.Location = new Point(0, 140);
+            btnTorneos.Name = "btnTorneos";
+            btnTorneos.Padding = new Padding(12, 0, 0, 0);
+            btnTorneos.Size = new Size(207, 60);
+            btnTorneos.TabIndex = 2;
+            btnTorneos.Text = "  Torneos";
+            btnTorneos.TextAlign = ContentAlignment.MiddleLeft;
+            btnTorneos.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnTorneos.UseVisualStyleBackColor = true;
+            btnTorneos.Click += btnTorneos_Click;
+            // 
+            // btnUsuarios
+            // 
+            btnUsuarios.Dock = DockStyle.Top;
+            btnUsuarios.FlatAppearance.BorderSize = 0;
+            btnUsuarios.FlatStyle = FlatStyle.Flat;
+            btnUsuarios.Font = new Font("Segoe UI", 10F);
+            btnUsuarios.ForeColor = Color.Gainsboro;
+            btnUsuarios.Image = (Image)resources.GetObject("btnUsuarios.Image");
+            btnUsuarios.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUsuarios.Location = new Point(0, 80);
+            btnUsuarios.Name = "btnUsuarios";
+            btnUsuarios.Padding = new Padding(12, 0, 0, 0);
+            btnUsuarios.Size = new Size(207, 60);
+            btnUsuarios.TabIndex = 1;
+            btnUsuarios.Text = "  Usuarios";
+            btnUsuarios.TextAlign = ContentAlignment.MiddleLeft;
+            btnUsuarios.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnUsuarios.UseVisualStyleBackColor = true;
+            btnUsuarios.Click += btnUsuarios_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Controls.Add(label1);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(207, 80);
+            panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.ForeColor = Color.LightGray;
+            label1.Location = new Point(12, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 21);
+            label1.TabIndex = 0;
+            label1.Text = "AGE Gestion de Torneos";
+            // 
+            // panelTitleBar
+            // 
+            panelTitleBar.BackColor = Color.FromArgb(0, 150, 136);
+            panelTitleBar.Controls.Add(btnMinimize);
+            panelTitleBar.Controls.Add(btnMaximize);
+            panelTitleBar.Controls.Add(btnClose);
+            panelTitleBar.Controls.Add(btnCloseChildForm);
+            panelTitleBar.Controls.Add(lblTitle);
+            panelTitleBar.Dock = DockStyle.Top;
+            panelTitleBar.Location = new Point(207, 0);
+            panelTitleBar.Name = "panelTitleBar";
+            panelTitleBar.Size = new Size(694, 80);
+            panelTitleBar.TabIndex = 1;
+            panelTitleBar.MouseDown += panelTitleBar_MouseDown;
+            // 
+            // btnMinimize
+            // 
+            btnMinimize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimize.FlatAppearance.BorderSize = 0;
+            btnMinimize.FlatStyle = FlatStyle.Flat;
+            btnMinimize.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMinimize.ForeColor = Color.White;
+            btnMinimize.Location = new Point(592, 0);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(30, 30);
+            btnMinimize.TabIndex = 4;
+            btnMinimize.Text = "O";
+            btnMinimize.UseVisualStyleBackColor = true;
+            btnMinimize.Click += btnMinimize_Click;
+            // 
+            // btnMaximize
+            // 
+            btnMaximize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMaximize.FlatAppearance.BorderSize = 0;
+            btnMaximize.FlatStyle = FlatStyle.Flat;
+            btnMaximize.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMaximize.ForeColor = Color.White;
+            btnMaximize.Location = new Point(628, 0);
+            btnMaximize.Name = "btnMaximize";
+            btnMaximize.Size = new Size(30, 30);
+            btnMaximize.TabIndex = 3;
+            btnMaximize.Text = "O";
+            btnMaximize.UseVisualStyleBackColor = true;
+            btnMaximize.Click += btnMaximize_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnClose.ForeColor = Color.White;
+            btnClose.Location = new Point(664, 0);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(30, 30);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "O";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnCloseChildForm
+            // 
+            btnCloseChildForm.Dock = DockStyle.Left;
+            btnCloseChildForm.FlatAppearance.BorderSize = 0;
+            btnCloseChildForm.FlatStyle = FlatStyle.Flat;
+            btnCloseChildForm.Image = Properties.Resources.cross_out__2_;
+            btnCloseChildForm.Location = new Point(0, 0);
+            btnCloseChildForm.Name = "btnCloseChildForm";
+            btnCloseChildForm.Size = new Size(75, 80);
+            btnCloseChildForm.TabIndex = 1;
+            btnCloseChildForm.UseVisualStyleBackColor = true;
+            btnCloseChildForm.Click += btnCloseChildForm_Click;
+            // 
+            // lblTitle
+            // 
+            lblTitle.Anchor = AnchorStyles.None;
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Miriam CLM", 16F);
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(312, 30);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(66, 24);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "MENU";
+            // 
+            // panelDesktopPane
+            // 
+            panelDesktopPane.Dock = DockStyle.Fill;
+            panelDesktopPane.Location = new Point(207, 80);
+            panelDesktopPane.Name = "panelDesktopPane";
+            panelDesktopPane.Size = new Size(694, 462);
+            panelDesktopPane.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(901, 542);
+            Controls.Add(panelDesktopPane);
+            Controls.Add(panelTitleBar);
+            Controls.Add(panelMenu);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panelMenu.ResumeLayout(false);
+            panelLogo.ResumeLayout(false);
+            panelLogo.PerformLayout();
+            panelTitleBar.ResumeLayout(false);
+            panelTitleBar.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Panel panelLogo;
+        private Button btnUsuarios;
+        private Button btnTorneos;
+        private Panel panelTitleBar;
+        private Label lblTitle;
+        private Label label1;
+        private Panel panelDesktopPane;
+        private Button btnCloseChildForm;
+        private Button btnClose;
+        private Button btnMinimize;
+        private Button btnMaximize;
     }
 }
---
+++ proyTorneos/Escritorio/FormUsuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escritorio
+{
+    public partial class FormUsuario : Form
+    {
+        public FormUsuario()
+        {
+            InitializeComponent();
+            //LoadTheme(); por alguna razon no funciona como corresponde por lo tanto lo dejo así
+        }
+
+        private void LoadTheme()
+        {
+            foreach (Control btns in this.Controls)
+            {
+                if (btns.GetType() == typeof(Button))
+                {
+                    Button btn = (Button)btns;
+                    btns.BackColor = ThemeColor.PrimaryColor;
+                    btns.ForeColor = Color.White;
+                    btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                }
+            }
+            label5.ForeColor = ThemeColor.SecondaryColor;
+            label6.ForeColor = ThemeColor.PrimaryColor;
+        }
+    }
+}
