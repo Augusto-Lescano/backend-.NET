7415daabc1095670b94db281b4f7321c409242e3
Augusto Lescano <164239807+Augusto-Lescano@users.noreply.github.com>
2025-08-02 23:54:31 -0300

    feature: agrega crud Torneo (#5)

---
+++ proyTorneos/DTOs/Torneo.cs
@@ -0,0 +1,17 @@
+﻿namespace DTOs
+{
+    public class Torneo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string DescripcionDeReglas { get; set; }
+        public int CantidadDeJugadores { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFin { get; set; }
+        public string FechaInicioDeInscripciones { get; set; }
+        public string FechaFinDeInscripciones { get; set; }
+        public string Resultado { get; set; }
+        public string Region { get; set; }
+        public string Estado { get; set; }
+    }
+}
