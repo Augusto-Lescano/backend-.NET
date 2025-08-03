d571c89e063b13835fc5aa1b1df1f3b38bc5e883
Gian Marco <104176205+GianMarcoMercanzini@users.noreply.github.com>
2025-08-03 13:42:39 -0300

    consumo de api rest con form de usuario

--- proyTorneos/Escritorio/FormUsuario.Designer.cs
+++ proyTorneos/Escritorio/FormUsuario.Designer.cs
@@ -30,27 +30,25 @@
         {
             button1 = new Button();
             label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            txtId = new TextBox();
+            txtNombre = new TextBox();
             label2 = new Label();
-            textBox3 = new TextBox();
+            txtApellido = new TextBox();
             label3 = new Label();
-            textBox4 = new TextBox();
+            txtEmail = new TextBox();
             label4 = new Label();
             label5 = new Label();
             label6 = new Label();
-            radioButton1 = new RadioButton();
-            checkBox1 = new CheckBox();
-            dataGridView1 = new DataGridView();
+            dgvUsuarios = new DataGridView();
             button2 = new Button();
             button3 = new Button();
-            textBox5 = new TextBox();
+            txtPais = new TextBox();
             label7 = new Label();
-            textBox6 = new TextBox();
+            txtGamerTag = new TextBox();
             label8 = new Label();
-            textBox7 = new TextBox();
+            txtRol = new TextBox();
             label9 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -60,138 +58,120 @@
             button1.Name = "button1";
             button1.Size = new Size(80, 34);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Agregar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += btnAgregar_Click;
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label1.AutoSize = true;
-            label1.Location = new Point(52, 54);
+            label1.Location = new Point(43, 85);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(18, 15);
             label1.TabIndex = 3;
-            label1.Text = "label1";
+            label1.Text = "ID";
             // 
-            // textBox1
+            // txtId
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox1.Location = new Point(96, 51);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 4;
+            txtId.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtId.Location = new Point(109, 82);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(100, 23);
+            txtId.TabIndex = 4;
             // 
-            // textBox2
+            // txtNombre
             // 
-            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox2.Location = new Point(96, 92);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 6;
+            txtNombre.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtNombre.Location = new Point(109, 123);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 6;
             // 
             // label2
             // 
             label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label2.AutoSize = true;
-            label2.Location = new Point(52, 95);
+            label2.Location = new Point(43, 126);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 5;
-            label2.Text = "label2";
+            label2.Text = "Nombre";
             // 
-            // textBox3
+            // txtApellido
             // 
-            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox3.Location = new Point(96, 135);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 8;
+            txtApellido.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtApellido.Location = new Point(109, 166);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(100, 23);
+            txtApellido.TabIndex = 8;
             // 
             // label3
             // 
             label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label3.AutoSize = true;
-            label3.Location = new Point(52, 138);
+            label3.Location = new Point(43, 169);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(51, 15);
             label3.TabIndex = 7;
-            label3.Text = "label3";
+            label3.Text = "Apellido";
             // 
-            // textBox4
+            // txtEmail
             // 
-            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox4.Location = new Point(96, 180);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
-            textBox4.TabIndex = 10;
+            txtEmail.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtEmail.Location = new Point(109, 211);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(100, 23);
+            txtEmail.TabIndex = 10;
             // 
             // label4
             // 
             label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label4.AutoSize = true;
-            label4.Location = new Point(52, 183);
+            label4.Location = new Point(43, 214);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
+            label4.Size = new Size(36, 15);
             label4.TabIndex = 9;
-            label4.Text = "label4";
+            label4.Text = "Email";
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Location = new Point(410, 23);
             label5.Name = "label5";
-            label5.Size = new Size(38, 15);
+            label5.Size = new Size(94, 15);
             label5.TabIndex = 11;
-            label5.Text = "label5";
+            label5.Text = "Lista de usuarios";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(52, 350);
+            label6.Location = new Point(43, 23);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(98, 15);
             label6.TabIndex = 12;
-            label6.Text = "label6";
+            label6.Text = "Datos del usuario";
             // 
-            // radioButton1
+            // dgvUsuarios
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(52, 384);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(94, 19);
-            radioButton1.TabIndex = 13;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
-            radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(52, 409);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(83, 19);
-            checkBox1.TabIndex = 14;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(410, 52);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(348, 332);
-            dataGridView1.TabIndex = 15;
+            dgvUsuarios.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUsuarios.Location = new Point(410, 52);
+            dgvUsuarios.Name = "dgvUsuarios";
+            dgvUsuarios.Size = new Size(348, 332);
+            dgvUsuarios.TabIndex = 15;
+            dgvUsuarios.SelectionChanged += dgvUsuarios_SelectionChanged;
             // 
             // button2
             // 
             button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            button2.Location = new Point(542, 398);
+            button2.Location = new Point(541, 398);
             button2.Name = "button2";
             button2.Size = new Size(80, 34);
             button2.TabIndex = 16;
-            button2.Text = "button2";
+            button2.Text = "Actualizar";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += btnActualizar_Click;
             // 
             // button3
             // 
@@ -200,93 +180,93 @@
             button3.Name = "button3";
             button3.Size = new Size(80, 34);
             button3.TabIndex = 17;
-            button3.Text = "button3";
+            button3.Text = "Eliminar";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += btnEliminar_Click;
             // 
-            // textBox5
+            // txtPais
             // 
-            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox5.Location = new Point(96, 218);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(100, 23);
-            textBox5.TabIndex = 19;
+            txtPais.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtPais.Location = new Point(109, 249);
+            txtPais.Name = "txtPais";
+            txtPais.Size = new Size(100, 23);
+            txtPais.TabIndex = 19;
             // 
             // label7
             // 
             label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label7.AutoSize = true;
-            label7.Location = new Point(52, 221);
+            label7.Location = new Point(43, 252);
             label7.Name = "label7";
-            label7.Size = new Size(38, 15);
+            label7.Size = new Size(28, 15);
             label7.TabIndex = 18;
-            label7.Text = "label7";
+            label7.Text = "País";
             // 
-            // textBox6
+            // txtGamerTag
             // 
-            textBox6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox6.Location = new Point(96, 258);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(100, 23);
-            textBox6.TabIndex = 21;
+            txtGamerTag.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtGamerTag.Location = new Point(109, 289);
+            txtGamerTag.Name = "txtGamerTag";
+            txtGamerTag.Size = new Size(100, 23);
+            txtGamerTag.TabIndex = 21;
             // 
             // label8
             // 
             label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label8.AutoSize = true;
-            label8.Location = new Point(52, 261);
+            label8.Location = new Point(43, 292);
             label8.Name = "label8";
-            label8.Size = new Size(38, 15);
+            label8.Size = new Size(60, 15);
             label8.TabIndex = 20;
-            label8.Text = "label8";
+            label8.Text = "GamerTag";
             // 
-            // textBox7
+            // txtRol
             // 
-            textBox7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            textBox7.Location = new Point(96, 296);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(100, 23);
-            textBox7.TabIndex = 23;
+            txtRol.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            txtRol.Location = new Point(109, 327);
+            txtRol.Name = "txtRol";
+            txtRol.Size = new Size(100, 23);
+            txtRol.TabIndex = 23;
             // 
             // label9
             // 
             label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label9.AutoSize = true;
-            label9.Location = new Point(52, 299);
+            label9.Location = new Point(43, 330);
             label9.Name = "label9";
-            label9.Size = new Size(38, 15);
+            label9.Size = new Size(24, 15);
             label9.TabIndex = 22;
-            label9.Text = "label9";
+            label9.Text = "Rol";
             // 
             // FormUsuario
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox7);
+            Controls.Add(txtRol);
             Controls.Add(label9);
-            Controls.Add(textBox6);
+            Controls.Add(txtGamerTag);
             Controls.Add(label8);
-            Controls.Add(textBox5);
+            Controls.Add(txtPais);
             Controls.Add(label7);
             Controls.Add(button3);
             Controls.Add(button2);
-            Controls.Add(dataGridView1);
-            Controls.Add(checkBox1);
-            Controls.Add(radioButton1);
+            Controls.Add(dgvUsuarios);
             Controls.Add(label6);
             Controls.Add(label5);
-            Controls.Add(textBox4);
+            Controls.Add(txtEmail);
             Controls.Add(label4);
-            Controls.Add(textBox3);
+            Controls.Add(txtApellido);
             Controls.Add(label3);
-            Controls.Add(textBox2);
+            Controls.Add(txtNombre);
             Controls.Add(label2);
-            Controls.Add(textBox1);
+            Controls.Add(txtId);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "FormUsuario";
             Text = "USUARIOS";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += FormUsuario_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -295,25 +275,23 @@
 
         private Button button1;
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox txtId;
+        private TextBox txtNombre;
         private Label label2;
-        private TextBox textBox3;
+        private TextBox txtApellido;
         private Label label3;
-        private TextBox textBox4;
+        private TextBox txtEmail;
         private Label label4;
         private Label label5;
         private Label label6;
-        private RadioButton radioButton1;
-        private CheckBox checkBox1;
-        private DataGridView dataGridView1;
+        private DataGridView dgvUsuarios;
         private Button button2;
         private Button button3;
-        private TextBox textBox5;
+        private TextBox txtPais;
         private Label label7;
-        private TextBox textBox6;
+        private TextBox txtGamerTag;
         private Label label8;
-        private TextBox textBox7;
+        private TextBox txtRol;
         private Label label9;
     }
 }
