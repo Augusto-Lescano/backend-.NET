84238de4897362beff3ee8f01b173905f3543e8d
Augusto Lescano <164239807+Augusto-Lescano@users.noreply.github.com>
2025-08-02 23:54:31 -0300

    feature: agrega crud Torneo (#5)

---
+++ proyTorneos/Domain.Model/Torneo.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Model
+{
+    public class Torneo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string DescripcionDeReglas { get; set; }
+        public int CantidadDeJugadores { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFin { get; set;  }
+        public string FechaInicioDeInscripciones { get; set; }
+        public string FechaFinDeInscripciones { get; set; }
+        public string Resultado { get; set; }   
+        public string Region { get; set; }
+        public string Estado { get; set; }
+
+        public Torneo(int id, string nombre, string descripcionDeReglas, int cantidadDeJugadores, string fechaDeInicio, string fechaFin, string fechaInicioDeInscripciones, string fechaFinDeInscripciones, string resultado, string region, string estado) {
+            Id = id;
+            Nombre = nombre;
+            DescripcionDeReglas = descripcionDeReglas;
+            CantidadDeJugadores = cantidadDeJugadores;
+            FechaInicio = fechaDeInicio;
+            FechaFin = fechaFin;
+            FechaInicioDeInscripciones = fechaInicioDeInscripciones;
+            FechaFinDeInscripciones = fechaFinDeInscripciones;
+            Resultado = resultado;
+            Region = region;
+            Estado = estado;
+        }
+
+
+    }
+}
