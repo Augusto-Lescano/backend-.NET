babed444969d3aea9b73f174389c4b9d691ef76e
Augusto Lescano <augustolescano15@gmail.com>
2025-08-13 17:28:00 -0300

    improve: se mejoro visualmente el winform de torneo

--- proyTorneos/DTOs/Torneo.cs
+++ proyTorneos/DTOs/Torneo.cs
@@ -6,10 +6,10 @@
         public string Nombre { get; set; }
         public string DescripcionDeReglas { get; set; }
         public int CantidadDeJugadores { get; set; }
-        public string FechaInicio { get; set; }
-        public string FechaFin { get; set; }
-        public string FechaInicioDeInscripciones { get; set; }
-        public string FechaFinDeInscripciones { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public DateTime FechaInicioDeInscripciones { get; set; }
+        public DateTime FechaFinDeInscripciones { get; set; }
         public string Resultado { get; set; }
         public string Region { get; set; }
         public string Estado { get; set; }
