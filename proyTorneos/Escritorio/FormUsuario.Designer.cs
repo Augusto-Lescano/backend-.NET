5cdfecd35119ae83cda381b500330625d09d651c
Augusto Lescano <164239807+Augusto-Lescano@users.noreply.github.com>
2025-08-13 15:10:56 -0300

    bug-fix: se arreglo error visual en los dgv donde las columnas no ocupaban todo el ancho (#8)

--- proyTorneos/Escritorio/FormUsuario.Designer.cs
+++ proyTorneos/Escritorio/FormUsuario.Designer.cs
@@ -53,10 +53,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(469, 531);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(410, 398);
             button1.Name = "button1";
-            button1.Size = new Size(91, 45);
+            button1.Size = new Size(80, 34);
             button1.TabIndex = 0;
             button1.Text = "Agregar";
             button1.UseVisualStyleBackColor = true;
@@ -66,94 +66,91 @@
             // 
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label1.AutoSize = true;
-            label1.Location = new Point(49, 113);
+            label1.Location = new Point(43, 85);
             label1.Name = "label1";
-            label1.Size = new Size(24, 20);
+            label1.Size = new Size(18, 15);
             label1.TabIndex = 3;
             label1.Text = "ID";
             // 
             // txtId
             // 
             txtId.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtId.Location = new Point(125, 109);
-            txtId.Margin = new Padding(3, 4, 3, 4);
+            txtId.Location = new Point(109, 82);
             txtId.Name = "txtId";
-            txtId.Size = new Size(114, 27);
+            txtId.Size = new Size(100, 23);
             txtId.TabIndex = 4;
             txtId.TextChanged += txtId_TextChanged;
             // 
             // txtNombre
             // 
             txtNombre.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtNombre.Location = new Point(125, 164);
-            txtNombre.Margin = new Padding(3, 4, 3, 4);
+            txtNombre.Location = new Point(109, 123);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(114, 27);
+            txtNombre.Size = new Size(100, 23);
             txtNombre.TabIndex = 6;
             // 
             // label2
             // 
             label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label2.AutoSize = true;
-            label2.Location = new Point(49, 168);
+            label2.Location = new Point(43, 126);
             label2.Name = "label2";
-            label2.Size = new Size(64, 20);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 5;
             label2.Text = "Nombre";
             // 
             // txtApellido
             // 
             txtApellido.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtApellido.Location = new Point(125, 221);
-            txtApellido.Margin = new Padding(3, 4, 3, 4);
+            txtApellido.Location = new Point(109, 166);
             txtApellido.Name = "txtApellido";
-            txtApellido.Size = new Size(114, 27);
+            txtApellido.Size = new Size(100, 23);
             txtApellido.TabIndex = 8;
             // 
             // label3
             // 
             label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label3.AutoSize = true;
-            label3.Location = new Point(49, 225);
+            label3.Location = new Point(43, 169);
             label3.Name = "label3";
-            label3.Size = new Size(66, 20);
+            label3.Size = new Size(51, 15);
             label3.TabIndex = 7;
             label3.Text = "Apellido";
             // 
             // txtEmail
             // 
             txtEmail.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtEmail.Location = new Point(125, 281);
-            txtEmail.Margin = new Padding(3, 4, 3, 4);
+            txtEmail.Location = new Point(109, 211);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(114, 27);
+            txtEmail.Size = new Size(100, 23);
             txtEmail.TabIndex = 10;
             // 
             // label4
             // 
             label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label4.AutoSize = true;
-            label4.Location = new Point(49, 285);
+            label4.Location = new Point(43, 214);
             label4.Name = "label4";
-            label4.Size = new Size(46, 20);
+            label4.Size = new Size(36, 15);
             label4.TabIndex = 9;
             label4.Text = "Email";
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label5.AutoSize = true;
-            label5.Location = new Point(469, 31);
+            label5.Location = new Point(238, 23);
             label5.Name = "label5";
-            label5.Size = new Size(118, 20);
+            label5.Size = new Size(94, 15);
             label5.TabIndex = 11;
             label5.Text = "Lista de usuarios";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(49, 31);
+            label6.Location = new Point(43, 23);
             label6.Name = "label6";
-            label6.Size = new Size(125, 20);
+            label6.Size = new Size(98, 15);
             label6.TabIndex = 12;
             label6.Text = "Datos del usuario";
             // 
@@ -161,21 +158,20 @@
             // 
             dgvUsuarios.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvUsuarios.Location = new Point(469, 69);
-            dgvUsuarios.Margin = new Padding(3, 4, 3, 4);
+            dgvUsuarios.Location = new Point(238, 52);
             dgvUsuarios.Name = "dgvUsuarios";
             dgvUsuarios.RowHeadersWidth = 51;
-            dgvUsuarios.Size = new Size(398, 443);
+            dgvUsuarios.Size = new Size(550, 332);
             dgvUsuarios.TabIndex = 15;
             dgvUsuarios.CellContentClick += dgvUsuarios_CellContentClick;
             dgvUsuarios.SelectionChanged += dgvUsuarios_SelectionChanged;
             // 
             // button2
             // 
-            button2.Location = new Point(618, 531);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.Location = new Point(541, 398);
             button2.Name = "button2";
-            button2.Size = new Size(91, 45);
+            button2.Size = new Size(80, 34);
             button2.TabIndex = 16;
             button2.Text = "Actualizar";
             button2.UseVisualStyleBackColor = true;
@@ -183,10 +179,10 @@
             // 
             // button3
             // 
-            button3.Location = new Point(775, 531);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.Location = new Point(678, 398);
             button3.Name = "button3";
-            button3.Size = new Size(91, 45);
+            button3.Size = new Size(80, 34);
             button3.TabIndex = 17;
             button3.Text = "Eliminar";
             button3.UseVisualStyleBackColor = true;
@@ -195,65 +191,62 @@
             // txtPais
             // 
             txtPais.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtPais.Location = new Point(125, 332);
-            txtPais.Margin = new Padding(3, 4, 3, 4);
+            txtPais.Location = new Point(109, 249);
             txtPais.Name = "txtPais";
-            txtPais.Size = new Size(114, 27);
+            txtPais.Size = new Size(100, 23);
             txtPais.TabIndex = 19;
             // 
             // label7
             // 
             label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label7.AutoSize = true;
-            label7.Location = new Point(49, 336);
+            label7.Location = new Point(43, 252);
             label7.Name = "label7";
-            label7.Size = new Size(34, 20);
+            label7.Size = new Size(28, 15);
             label7.TabIndex = 18;
             label7.Text = "País";
             // 
             // txtGamerTag
             // 
             txtGamerTag.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtGamerTag.Location = new Point(125, 385);
-            txtGamerTag.Margin = new Padding(3, 4, 3, 4);
+            txtGamerTag.Location = new Point(109, 289);
             txtGamerTag.Name = "txtGamerTag";
-            txtGamerTag.Size = new Size(114, 27);
+            txtGamerTag.Size = new Size(100, 23);
             txtGamerTag.TabIndex = 21;
             // 
             // label8
             // 
             label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label8.AutoSize = true;
-            label8.Location = new Point(49, 389);
+            label8.Location = new Point(43, 292);
             label8.Name = "label8";
-            label8.Size = new Size(76, 20);
+            label8.Size = new Size(60, 15);
             label8.TabIndex = 20;
             label8.Text = "GamerTag";
             // 
             // txtRol
             // 
             txtRol.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            txtRol.Location = new Point(125, 436);
-            txtRol.Margin = new Padding(3, 4, 3, 4);
+            txtRol.Location = new Point(109, 327);
             txtRol.Name = "txtRol";
-            txtRol.Size = new Size(114, 27);
+            txtRol.Size = new Size(100, 23);
             txtRol.TabIndex = 23;
             // 
             // label9
             // 
             label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             label9.AutoSize = true;
-            label9.Location = new Point(49, 440);
+            label9.Location = new Point(43, 330);
             label9.Name = "label9";
-            label9.Size = new Size(31, 20);
+            label9.Size = new Size(24, 15);
             label9.TabIndex = 22;
             label9.Text = "Rol";
             // 
             // FormUsuario
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(txtRol);
             Controls.Add(label9);
             Controls.Add(txtGamerTag);
@@ -274,7 +267,6 @@
             Controls.Add(txtId);
             Controls.Add(label1);
             Controls.Add(button1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "FormUsuario";
             Text = "USUARIOS";
             Load += FormUsuario_Load;
---
+++ proyTorneos/Escritorio/Shared.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escritorio
+{
+    public static class Shared
+    {
+        public static void AjustarDataGridView(DataGridView tabla)
+        {
+            tabla.RowHeadersVisible = false;
+            tabla.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tabla.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            tabla.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            tabla.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; 
+        }
+    }
+}
