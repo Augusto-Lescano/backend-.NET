c7e704cbcfb0dc3d586896543458293adb43da6d
Augusto Lescano <augustolescano15@gmail.com>
2025-08-15 10:13:09 -0300

    improve: Se mejoraron los nombres de las columnas de la lista de torneos

--- proyTorneos/Escritorio/FormTorneo.cs
+++ proyTorneos/Escritorio/FormTorneo.cs
@@ -46,6 +46,7 @@ namespace Escritorio
         {
             
             await CargarTorneosAsync();
+            MejorarCabeceras();
             Shared.AjustarDataGridView(dgvListaTorneos);
         }
 
@@ -168,6 +169,15 @@ namespace Escritorio
             {
                 MessageBox.Show("Selecciona un torneo de la grilla");
             }
+        }
+        private void MejorarCabeceras() {
+            dgvListaTorneos.Columns[2].HeaderText = "Descripcion de reglas";
+            dgvListaTorneos.Columns[3].HeaderText = "Cantidad de jugadores";
+            dgvListaTorneos.Columns[4].HeaderText = "Fecha de Inicio";
+            dgvListaTorneos.Columns[5].HeaderText = "Fecha de Fin";
+            dgvListaTorneos.Columns[6].HeaderText = "Fecha de Inicio de Inscripciones";
+            dgvListaTorneos.Columns[7].HeaderText = "Fecha de Fin de Inscripciones";
+
         }
     }
 }
