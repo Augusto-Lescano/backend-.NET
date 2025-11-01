2b80ee22a65d428b352f708479ccf699f5fabae3
Jose Emilio Certo <164695697+emilioooc@users.noreply.github.com>
2025-11-01 10:55:06 -0300

    Arregla bug crear torneo e inscripcion (#37)

--- proyTorneos/Data/InscripcionRepository.cs
+++ proyTorneos/Data/InscripcionRepository.cs
@@ -14,6 +14,7 @@ namespace Data
 
         public void Add(Inscripcion inscripcion)
         {
+
             using var context = CreateContext();
             context.Inscripciones.Add(inscripcion);
             context.SaveChanges();
