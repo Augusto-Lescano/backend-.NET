72fa9145bb14856e15b16e6ccd0411a3b6e99930
Jose Emilio Certo <164695697+emilioooc@users.noreply.github.com>
2025-11-03 16:36:39 -0300

    Configura Blazor Server (#42)

---
+++ proyTorneos/BlazorApp/Program.cs
@@ -0,0 +1,28 @@
+using API.Clients;
+using Domain.Services;
+using BlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<TorneoApiClient>();
+builder.Services.AddScoped<TorneoService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
