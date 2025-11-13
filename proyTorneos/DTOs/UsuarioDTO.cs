40a37bf12f3839a36da23e6ef891b8b8e3e5e268
Augusto Lescano <augustolescano15@gmail.com>
2025-11-13 19:51:46 -0300

    add: se agrega pagina de equipos y detalles
    se agrega pagina de equipos, se agregan nuevos iconos en el nav bar, se
    agrega acceso a los datos mediante el api clients accediendo desde cada
    una de las paginas .razor

--- proyTorneos/DTOs/UsuarioDTO.cs
+++ proyTorneos/DTOs/UsuarioDTO.cs
@@ -1,15 +1,28 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace DTOs
 {
     public class UsuarioDTO
     {
         public int Id { get; set; }
+     
         public string Nombre { get; set; } = string.Empty;
+        
+        
         public string Apellido { get; set; } = string.Empty;
+        
+        
         public string Email { get; set; } = string.Empty;
+
+        
         public string Clave {  get; set; } = string.Empty;
+
+        
         public string Pais { get; set; } = string.Empty;
+
+        
         public string NombreUsuario { get; set; } = string.Empty;
         public DateTime FechaAlta { get; set; }
         public bool Admin { get; set; }
--- proyTorneos/Domain.Model/Usuario.cs
+++ proyTorneos/Domain.Model/Usuario.cs
@@ -2,10 +2,12 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
+
 namespace Domain.Model
 {
     public class Usuario
     {
+        
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
